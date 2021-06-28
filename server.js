4ea3c5d73744522432afaa7f33cd11583ac21fe1
Jacques <jacquesvst@gmail.com>
2021-06-28 16:22:02 -0300

    files

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: 'https://jacquesvst.github.io/',
+        transports: ['websocket', 'polling'],
+        methods: ["GET", "POST"],
+        credentials: true
+      },
+      allowEIO3: true
+});
+const port = 25565;
+
+
+app.use(express.static(__dirname + '/public'));
+
+function onConnection(socket){
+  console.log('New connection: ' + socket.id);
+  socket.on('drawing', (data) => {
+      socket.broadcast.emit('drawing', data);
+    });
+}
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('listening on port ' + port));
